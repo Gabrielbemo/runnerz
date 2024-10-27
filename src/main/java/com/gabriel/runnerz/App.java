a8064195372ebdc3bdb2267a3fc6cbe10940567c
Gabrielbemo <gabrielbemoura@gmail.com>
2024-10-27 13:09:45 -0300

    added postgresql as database and refactoring all classes to support it

--- src/main/java/com/gabriel/runnerz/App.java
+++ src/main/java/com/gabriel/runnerz/App.java
@@ -1,17 +1,9 @@
 package com.gabriel.runnerz;
 
-import com.gabriel.runnerz.run.Location;
-import com.gabriel.runnerz.run.Run;
-import com.gabriel.runnerz.run.RunRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class App {
