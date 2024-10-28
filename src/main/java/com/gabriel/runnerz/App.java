ad8bd647cee4f8f91c6d1a9770418dcdb4082b50
Gabrielbemo <gabrielbemoura@gmail.com>
2024-10-28 15:18:07 -0300

    added interface to httpClient with annotation GetExchange and setup

--- src/main/java/com/gabriel/runnerz/App.java
+++ src/main/java/com/gabriel/runnerz/App.java
@@ -2,6 +2,7 @@ package com.gabriel.runnerz;
 
 import com.gabriel.runnerz.run.RunRepository;
 import com.gabriel.runnerz.user.User;
+import com.gabriel.runnerz.user.UserHttpClient;
 import com.gabriel.runnerz.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.util.List;
 
@@ -22,13 +26,26 @@ public class App {
     }
 
     @Bean
-    CommandLineRunner runner(UserRestClient restClient) {
+    UserHttpClient userHttpClient(){
+        RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+        return factory.createClient(UserHttpClient.class);
+    }
+
+    @Bean
+    CommandLineRunner runner(UserRestClient restClient, UserHttpClient userHttpClient) {
         return args -> {
             List<User> users = restClient.findAll();
             System.out.println(users);
 
             User user = restClient.findById(1);
             System.out.println(user);
+
+            List<User> users2 = userHttpClient.findAll();
+            System.out.println(users2);
+
+            User user2 = userHttpClient.findById(1);
+            System.out.println(user2);
         };
     }
 }
