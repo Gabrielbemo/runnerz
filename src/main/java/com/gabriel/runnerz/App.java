827004d1b5507ad46c6147635e508191f1c6816f
Gabrielbemo <gabrielbemoura@gmail.com>
2024-10-28 15:07:26 -0300

    added simple calls with RestClient to represent users

--- src/main/java/com/gabriel/runnerz/App.java
+++ src/main/java/com/gabriel/runnerz/App.java
@@ -1,9 +1,16 @@
 package com.gabriel.runnerz;
 
+import com.gabriel.runnerz.run.RunRepository;
+import com.gabriel.runnerz.user.User;
+import com.gabriel.runnerz.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class App {
@@ -14,12 +21,14 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
-//    @Bean
-//    CommandLineRunner runner(RunRepository runRepository) {
-//        return args -> {
-//            Run run = new Run(1, "First run", LocalDateTime.now(), LocalDateTime.now()
-//                    .plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
-//            runRepository.create(run);
-//        };
-//    }
+    @Bean
+    CommandLineRunner runner(UserRestClient restClient) {
+        return args -> {
+            List<User> users = restClient.findAll();
+            System.out.println(users);
+
+            User user = restClient.findById(1);
+            System.out.println(user);
+        };
+    }
 }
